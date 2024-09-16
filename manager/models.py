e6937e6aabeef3d58ed19c1b2c75a53bcce16cb2
Raja <rdas6313@gmail.com>
2024-09-16 19:31:48 +0530

    add data models

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Mall(models.Model):
+    """ Here we define the data model for mall information """
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    customers = models.ManyToManyField('Customer')
+    inventories = models.ManyToManyField('Inventory')
+
+
+class Inventory(models.Model):
+    """ Data model for inventory information """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField()
+
+
+class Store(models.Model):
+    """ Data model for Store information """
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    lease_start = models.DateField(auto_now_add=True)
+    lease_end = models.DateField()
+    mall = models.ForeignKey(Mall, on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    """ Data model for employee data """
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    address = models.TextField()
+    mall = models.ForeignKey(Mall, on_delete=models.CASCADE)
+    store = models.ForeignKey(
+        Store, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Customer(models.Model):
+    """ Data model for customer data """
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    address = models.TextField()
+    last_visit = models.DateField()
