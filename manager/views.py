50a6a84c7df7173a807cd029d761236992f16fd1
Raja Das <rdas6313@gmail.com>
2024-09-28 18:00:13 +0530

    implemented pagination

--- manager/views.py
+++ manager/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from . import models
+from math import ceil
 
 
 def index(request, mall_id):
 
     table = request.GET.get('table', None)
     page = request.GET.get('page', 1)
-    page_size = 5
+    page_size = 2
     current_pages = {'store': 1, 'inventory': 1}
     if table is not None:
         current_pages[table] = int(page)
@@ -14,6 +15,7 @@ def index(request, mall_id):
 
     queryset = models.Store.objects.filter(
         mall=mall_id).values_list(*store_headers)
+    store_count = queryset.count()
     store_list = queryset[page_size *
                           (current_pages['store']-1): page_size * current_pages['store']]
 
@@ -22,11 +24,17 @@ def index(request, mall_id):
         mall=mall_id).values_list(*inventory_headers)
     inventory_list = queryset[page_size *
                               (current_pages['inventory']-1): page_size * (current_pages['inventory'])]
-
+    print(type(page))
     context = {
         'store': {
             'headers': store_headers,
-            'rows': store_list
+            'rows': store_list,
+            'page': {
+                'mall': mall_id,
+                'pages': [i+1 for i in range(ceil(store_count/page_size))],
+                'table': 'store',
+                'current_page': int(page)
+            }
         },
         'inventory': {
             'headers': inventory_headers,
